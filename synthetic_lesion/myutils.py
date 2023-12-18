18baf61010d51a8f04a5a01a28a6a644351942d6
Anand Joshi <ajoshi@.usc.edu>
2023-12-17 20:46:50 -0800

    updated

--- synthetic_lesion/myutils.py
+++ synthetic_lesion/myutils.py
@@ -3,7 +3,7 @@ import os
 import random
 import nibabel as nib
 from scipy import ndimage
-
+import glob
 
 def smooth_3d_segmentation_mask(input_path, output_path, iterations=1):
     # Read the 3D segmentation mask
@@ -59,6 +59,39 @@ def random_lesion_segmentation(brats_data_dir):
     return random_segmentation_file, random_t1_file
 
 
+# This function reads a random lesion segmentation file from the BRATS dataset
+def random_lesion_segmentation_carc(brats_data_dir):
+    """
+    Reads a random lesion segmentation file from the BRATS dataset.
+
+    Parameters:
+    - brats_data_dir (str): The directory path of the BRATS dataset.
+
+    Returns:
+    - random_segmentation_file (str): The file path of the randomly selected lesion segmentation file.
+        Returns None if no segmentation files are found.
+    """
+    # Get a list of subject directories in the BRATS dataset
+    subject_t1_files = glob.glob(brats_data_dir+'/t1/B*.nii.gz')
+
+    # Randomly select a subject directory
+    random_t1_file = random.choice(subject_t1_files)
+    _, t1_fname = os.path.split(random_t1_file)
+
+    random_subject = t1_fname[:-10]
+    # Get the segmentation file path (assuming the segmentation file is in the "seg" subdirectory)
+    random_segmentation_file = os.path.join(
+        brats_data_dir, 'seg',random_subject + "_seg.nii.gz"
+    )
+
+    # segmentation_files = [f for f in os.listdir(segmentation_dir) if f.endswith('.nii.gz')]
+
+    if (not os.path.isfile(random_segmentation_file)) or (not os.path.isfile(random_t1_file)) :
+        print(f"No segmentation files found for subject {random_segmentation_file}")
+        return None
+
+    return random_segmentation_file, random_t1_file
+
 # This function reads a random normal subject t1 file from the camcan dataset
 def random_normal_subject(norm_data_dir):
     """This function reads a random normal subject t1 file from the camcan dataset.
@@ -92,3 +125,34 @@ def random_normal_subject(norm_data_dir):
         return None
 
     return random_subject_file, random_subject_mask_file, random_subject
+
+
+
+# This function reads a random normal subject t1 file from the camcan dataset
+def random_normal_subject_carc(norm_data_dir):
+    """This function reads a random normal subject t1 file from the camcan dataset.
+
+    Args:
+        norm_data_dir (str): The directory path where the normal subject data is stored.
+
+    Returns:
+        tuple: A tuple containing the file paths of the randomly selected T1 file and its corresponding mask file.
+               If the T1 file or mask file is not found, None is returned.
+    """
+
+    # Get a list of subject directories in the BRATS dataset
+    sub_list = glob.glob(norm_data_dir+'/norm_IXI*_t1.nii.gz')
+
+    # Randomly select a subject directory
+    random_subject_file = random.choice(sub_list)
+
+    if (not os.path.isfile(random_subject_file)):
+        print(
+            f"T1 and/or mask file(s) found {random_subject_file}, {random_subject_mask_file}"
+        )
+        return None
+
+    _,base = os.path.split(random_subject_file)
+    random_subject = base[5:-10]
+
+    return random_subject_file, random_subject
