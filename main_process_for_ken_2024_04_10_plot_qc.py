7c808dd39bed786f226786f8c5ae2d0f14003dca
Anand A. Joshi <ajoshi@usc.edu>
2024-04-13 01:21:42 -0700

    update

---
+++ main_process_for_ken_2024_04_10_plot_qc.py
@@ -0,0 +1,74 @@
+
+import csv
+import glob
+
+from cv2 import Subdiv2D
+from requests import post
+from torch import layout
+from autoresec import delineate_resection, delineate_resection_post
+import os
+from nilearn.plotting import plot_anat, find_xyz_cut_coords
+from matplotlib import pyplot as plt
+
+sublist = glob.glob('/deneb_disk/auto_resection/seizure_free_patients_from_ken/2024_04_10_mri_dump/*')
+
+# Initialize an empty list to store subject IDs with preop MRI
+subjects_with_mri = []
+
+# Open the CSV file for reading
+for fname in sublist:
+    subid = os.path.basename(fname)
+    preop_mri = f'/deneb_disk/auto_resection/seizure_free_patients_from_ken/2024_04_10_mri_dump/{subid}/sMRI/sub-{subid}-{subid}_MRI.nii.gz'
+    postop_mri = f'/deneb_disk/auto_resection/seizure_free_patients_from_ken/2024_04_10_mri_dump/{subid}/sMRI/sub-{subid}-{subid}_post_RS_MRI.nii.gz'
+
+    if not os.path.isfile(postop_mri):
+        postop_mri = f'/deneb_disk/auto_resection/seizure_free_patients_from_ken/2024_04_10_mri_dump/{subid}/sMRI/sub-{subid}-{subid}_postRS_MRI.nii.gz'
+
+    # Check if the subject has preop MRI
+    if os.path.isfile(preop_mri):
+        print(f'Subject {subid} has preop MRI')
+    else:
+        print(f'File {preop_mri} does not exist')
+    
+    # Check if the subject has postop MRI
+    if os.path.isfile(postop_mri):
+        print(f'Subject {subid} has postop MRI')
+    else:
+        print(f'File {postop_mri} does not exist')           
+
+
+    # Check if both preop and postop MRI exist
+    if os.path.isfile(preop_mri) and os.path.isfile(postop_mri):
+
+
+        #if not os.path.isfile(postop_mri.replace('.nii.gz', '.resection.mask.nii.gz')):
+        #    delineate_resection_post(preop_mri, postop_mri)
+        
+        if os.path.isfile(preop_mri.replace('.nii.gz', '.resection.mask.nii.gz')):
+
+            resection_mask = preop_mri.replace('.nii.gz', '.resection.mask.nii.gz')
+            
+            fig, axes = plt.subplots(nrows=2, ncols=1) #, figsize=(14, 20))
+
+            #plot the preop mri with resection mask
+            cut_coords = find_xyz_cut_coords(resection_mask)
+            p = plot_anat(preop_mri, title=f'{subid} preop MRI with resection mask', axes=axes[0], cut_coords=cut_coords, vmax=500)
+            p.add_contours(resection_mask, levels=[.5], colors='r')
+            # also plot postop MRI in the same figure
+            plot_anat(postop_mri, title=f'{subid} postop MRI', axes=axes[1], cut_coords=cut_coords, vmax=500)
+            fig.savefig(f'/deneb_disk/auto_resection/seizure_free_patients_from_ken/2024_04_10_mri_dump/{subid}/sMRI/sub-{subid}-{subid}_MRI_resection.png')
+            plt.tight_layout()
+            #plt.show()
+            print(f'Plotted preop MRI with resection mask for {subid}')
+            plt.close()
+
+        print(f'Subject {subid} processed')
+
+
+
+print('Done')
+
+
+
+    
+
